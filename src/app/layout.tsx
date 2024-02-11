58f22b778582a352bbc2b513547c9d232b20e819
Emiliano <47641194+Emilianoac@users.noreply.github.com>
2024-02-11 12:01:14 -0300

    UPD: Change globlal styles file from CSS to SCSS.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./global.css"
+import "./global.scss"
 import ImportBsJS from "@/components/utils/ImportBsJS";
 import Navbar from "@/components/Navbar";
 
