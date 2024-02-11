327ac508776b6891aa54adabd669dbfa21ae10d0
Emiliano <47641194+Emilianoac@users.noreply.github.com>
2024-02-11 19:40:48 -0300

    ADD: Spinner loader

---
+++ src/app/loading.tsx
@@ -0,0 +1,10 @@
+import "./loading.scss"
+
+export default function Loading() {
+  // Or a custom loading skeleton component
+  return (
+    <div className="loader">
+      <div className="loader__spinner"></div>
+    </div>
+  )
+}
