6d89890c5ee7c5ef516f0e8fae8729891bdab245
Emiliano <47641194+Emilianoac@users.noreply.github.com>
2024-02-12 22:40:17 -0300

    ADD: Top Loader bar.

--- src/app/template.tsx
+++ src/app/template.tsx
@@ -1,5 +1,7 @@
 "use client"
 import { motion } from "framer-motion"
+import NextTopLoader from "nextjs-toploader";
+
 
 const variants = {
   hidden: { opacity: 0, x: -400, y: 0 },
@@ -8,6 +10,8 @@ const variants = {
 
 export default function Template({ children }: { children: React.ReactNode }) {
   return (
+    <>
+    <NextTopLoader />
     <motion.main
       variants={variants}
       initial="hidden"
@@ -16,5 +20,7 @@ export default function Template({ children }: { children: React.ReactNode }) {
     >
       {children}
     </motion.main>
+
+    </>
   )
 }
